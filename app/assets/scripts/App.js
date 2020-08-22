d0eb13281415c564ce3535b8e6147fbc10fe0b40
chienthangyalalt <ya1a1tapple@gmail.com>
2020-08-22 16:29:23 +0800

    some file commit and  edited

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,33 +1,6 @@
-import "../styles/styles.css";
-import "lazysizes";
-import MobileMenu from "./modules/MobileMenu";
-import RevealOnScroll from "./modules/RevealOnScroll";
-import StickyHeader from "./modules/StickyHeader";
-import ClientArea from "./modules/ClientArea";
+import("../styles/styles.css");
 
-new ClientArea();
-new StickyHeader();
-new RevealOnScroll(document.querySelectorAll(".feature-item"), 75);
-new RevealOnScroll(document.querySelectorAll(".testimonial"), 50);
-new MobileMenu();
-let modal;
-
-document.querySelectorAll(".open-modal").forEach(el => {
-  el.addEventListener("click", e => {
-    e.preventDefault();
-    if (typeof modal == "undefined") {
-      import(/* webpackChunkName: "modal" */ "./modules/Modal")
-        .then(x => {
-          modal = new x.default();
-          setTimeout(() => modal.openModal(), 20);
-        })
-        .catch(() => console.log("Файлыг дуудахад алдаа гарлаа"));
-    } else {
-      modal.openModal();
-    }
-  });
-});
-
+// alert("Welcome to Work, Mr. G.Yalalt");
 if (module.hot) {
   module.hot.accept();
 }
